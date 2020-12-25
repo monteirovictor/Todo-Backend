519d425d2aaba43263f83ef7a8887d09dd86bc24
monteirovictor <victormonteiromaia95@gmail.com>
2020-12-25 18:57:48 -0300

    backend

---
+++ src/middlewares/TaskValidation.js
@@ -0,0 +1,42 @@
+const TaskModel = require('../model/TaskModel');
+
+const { isPast } = require('date-fns');
+
+const TaskValidation = async(req, res, next) => {
+    const { macaddress, type, title, description, when } = req.body;
+    if (!macaddress)
+        return res.status(400).json({ error: 'macaddress é obrigatório' });
+    else if (!type)
+        return res.status(400).json({ error: 'type  é obrigatório' });
+    else if (!title)
+        return res.status(400).json({ error: 'title  é obrigatório' });
+    else if (!description)
+        return res.status(400).json({ error: 'description  é obrigatório' });
+    else if (!when)
+        return res.status(400).json({ error: 'when  é obrigatório' });
+    else if (isPast(new Date(when)))
+        return res.status(400).json({ error: 'Escolha uma data e hora futura' });
+    else {
+        let exists;
+
+        if (req.params.id) {
+            exists = await TaskModel.findOne({
+                '_id': { '$ne': req.params.id },
+                'when': { '$eq': new Date(when) },
+                'macaddress': { '$in': macaddress }
+            });
+        } else {
+
+            exists = await TaskModel.findOne({
+                'when': { '$eq': new Date(when) },
+                'macaddress': { '$in': macaddress }
+            });
+        }
+        if (exists) {
+            return res.status(400).json({ error: 'já existe uma tarefa neste dia e horário' });
+        }
+        next();
+    }
+}
+
+module.exports = TaskValidation;
